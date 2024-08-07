e419be85bb1f7bf1d00fbe84fb18696656d7de8e
Давид Заде <Davidzade21@mail.ru>
2024-08-07 18:24:05 +0300

    just deleted the few unnecessary files

--- feather/feather/setenv.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def setenvf():
-    os.environ["POSTGRES_DB"] = 'David'
-    os.environ["POSTGRES_USER"] = 'postgres2'
-    os.environ["POSTGRES_PASSWORD"] = 'David2004'
-    os.environ["DB_HOST"] = 'db'
